eb33ae1e25c7b8b49f760a2e98bf8b0cd339b4d4
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-26 16:26:58 -0400

    array of objects created and images added to the folder

---
+++ app.js
@@ -0,0 +1,39 @@
+const exercises = 
+[
+    {
+        id: 1,
+        img: "./Assets/images/benchpress.jpg",
+        name: "bench press",
+        muscleTarget: "chest",
+    },
+    {
+        id: 2,
+        img: "./Assets/images/pullover.png",
+        name: "pull over",
+        muscleTarget: "back",
+    },
+    {
+        id: 3,
+        img: "./Assets/images/squat.png",
+        name: "squats",
+        muscleTarget: "legs",
+    },
+    {
+        id: 4,
+        img: "./Assets/images/curlhammer.jpg",
+        name: "hammer curls",
+        muscleTarget: "biceps",
+    },
+    {
+        id: 5,
+        img: "./Assets/images/pushdown.jpg",
+        name: "push down",
+        muscleTarget: "triceps",
+    },
+    {
+        id: 6,
+        img: "./Assets/images/skullcrushers.png",
+        name: "skull crushers",
+        muscleTarget: "shoulders",
+    },
+]
