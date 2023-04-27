2693a8688a7100b34ee4f0bb3c9812a1114232aa
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-27 09:32:04 -0400

    documentation for the js file has been created as well as items are being retrieved to the html file

--- app.js
+++ app.js
@@ -37,3 +37,36 @@ const exercises =
         muscleTarget: "shoulders",
     },
 ]
+
+//target the main section (where the items are going to be displayed)
+
+const mainSection = document.querySelector('.main-section');
+
+//Retrieve the DOM (using DOMContentLoaded) when page is loaded
+window.addEventListener('DOMContentLoaded', function(){
+    displayExercisesItems(exercises);
+    console.log(displayExercises);
+});
+
+function displayExercisesItems(exerciseItems){
+    //Use map method in order to be able to modify our objects.
+    //let displayExercises = exercise.map(function(exercise) exercise keyword changed by exerciseItems as parameter to be passed.
+    let displayExercises = exerciseItems.map(function(exercise){
+        //template string to be able to write html code in js
+        //We start accessing the objects by each property.
+        return `<article class="exercise-item">
+        <img src=${exercise.img} alt="chest" class="graphic">
+        <div class="exercise-info">
+            <header>
+                <h4>${exercise.name}</h4>
+                <h4>${exercise.muscleTarget}</h4>
+            </header>
+            <h4>Exercise to add to your rutine</h4>
+        </div>
+    </article>`
+    });
+    //Use join method to place the item together
+    displayExercises = displayExercises.join('');
+    //Display items in the html document by selecting the parent that is mainSection and passing the displayExercises to it.
+    mainSection.innerHTML = displayExercises;
+};
