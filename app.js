cf7ee5be79cd51db41a028d7010ead8310c6146a
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-27 15:56:45 -0400

    buttons added dynamically to our html file

--- app.js
+++ app.js
@@ -44,10 +44,34 @@ const mainSection = document.querySelector('.main-section');
 //select all the buttons from our html file
 const buttonsFilter = document.querySelectorAll('.button-filter');
 
+//load items
 //Retrieve the DOM (using DOMContentLoaded) when page is loaded
 window.addEventListener('DOMContentLoaded', function(){
     displayExercisesItems(exercises);
-    console.log(displayExercises);
+});
+
+//filter items
+//We use a foreach loop in order to iterate in each button that we have
+buttonsFilter.forEach(function(btns){
+    btns.addEventListener('click', function(e){
+        //currentTarget and dataset added in order to match our data-id in our html file
+        //Whatever value data-id has in the html it has to match with property in our array
+        const muscleTargeted = e.currentTarget.dataset.id;
+        
+        // we use filter property in order to be able to access to each property in the "exercises" array
+        const muscleCategory = exercises.filter(function(exerciseItem){
+            if(exerciseItem.muscleTarget === muscleTargeted){
+                return exerciseItem;
+            }
+            // console.log(exerciseItem.muscleTarget) 
+        });
+        if(muscleTargeted === "all"){
+            displayExercisesItems(exercises);
+        }
+        else(
+            displayExercisesItems(muscleCategory)
+        )
+    });
 });
 
 function displayExercisesItems(exerciseItems){
